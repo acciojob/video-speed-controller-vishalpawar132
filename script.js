539a33263399014943c4d4ec3b6dd1c73d720e6c
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-07-23 10:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.flex');
+const speedBar = document.querySelector('.speed-bar');
+const speed = document.querySelector('.speed input');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  this.paused ? this.play() : this.pause();
+  this.parentNode.querySelector('.player__button').textContent = icon;
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function handleProgress() {
+  const percent = (this.currentTime / this.duration) * 100;
+  const progressBar = this.parentNode.querySelector('.progress__filled');
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function handleSpeedUpdate() {
+  video.playbackRate = speed.value;
+  speedBar.textContent = `${speed.value}×`;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleVolumeChange() {
+  video.volume = this.value;
+}
+
+function handleVideoMousemove(event) {
+  const scrubTime = (event.offsetX / video.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', () => video.parentNode.querySelector('.player__button').textContent = '❚ ❚');
+video.addEventListener('pause', () => video.parentNode.querySelector('.player__button').textContent = '►');
+video.addEventListener('timeupdate', handleProgress);
+
+speed.addEventListener('change', handleSpeedUpdate);
+speed.addEventListener('mousemove', handleSpeedUpdate);
+
+const skipButtons = document.querySelectorAll('[data-skip]');
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+const volume = document.querySelector('[name="volume"]');
+volume.addEventListener('change', handleVolumeChange);
+volume.addEventListener('mousemove', handleVolumeChange);
+
+const progress = document.querySelector('.progress');
+progress.addEventListener('mousemove', handleVideoMousemove);
